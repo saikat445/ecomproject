c539b959de99e7f263990f04ff92795bd4cbccac
saikat445 <81201597+saikat445@users.noreply.github.com>
2022-09-09 07:14:44 +0530

    Update settings.py
    static files setting changed

--- ecomproject/settings.py
+++ ecomproject/settings.py
@@ -135,9 +135,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR, "static"]
-STATIC_ROOT = BASE_DIR / "static_cdn"
+STATIC_ROOT = BASE_DIR, "static_cdn"
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = BASE_DIR, "media"
 
 
 

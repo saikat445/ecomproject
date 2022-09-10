63127522e3761bf73d8bd2bb4eb6fb11a855767b
saikat445 <81201597+saikat445@users.noreply.github.com>
2022-09-10 11:36:28 +0530

    Update settings.py
    setting change 3

--- ecomproject/settings.py
+++ ecomproject/settings.py
@@ -134,10 +134,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR, "static"]
-STATIC_ROOT = BASE_DIR, "static_cdn"
+#STATICFILES_DIRS = [BASE_DIR, "static"]
+#STATIC_ROOT = BASE_DIR, "static_cdn"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR, "media"
+#MEDIA_ROOT = BASE_DIR, "media"
+MEDIA_ROOT =os.path.join(BASE_DIR, 'static/')
 
 
 
